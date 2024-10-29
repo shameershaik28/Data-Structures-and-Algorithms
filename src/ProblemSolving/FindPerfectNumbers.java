c6d5be6304ec6d204505d3238902ccbff5a52ee7
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-29 23:25:56 +0530

    optimized the code, TC : O(sqrt(A)) SC: O(1)

--- src/ProblemSolving/FindPerfectNumbers.java
+++ src/ProblemSolving/FindPerfectNumbers.java
@@ -6,19 +6,20 @@ public class FindPerfectNumbers {
     public int solve(int A) {
         //Base Condition
         if(A<=0) return 0;
-        int sum =0;
-        for(int i=1; i<A;i++)
-        {
-            if(A%i==0)
-            {
-                sum = sum+i;
+        int sum =1; // 1 is the common divisor for all values
+        for(int i=2; i*i<=A;i++) {
+            if (A % i == 0) {
+                if(i==A/i)
+                {
+                    sum = sum + i;
+                }
+                else
+                {
+                    sum = sum + i+A/i;
+                }
             }
         }
-        if(sum==A)
-        {
-            return 1;
-        }
-        return 0;
+        return (sum==A)? 1 : 0;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
