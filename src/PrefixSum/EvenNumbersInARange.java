7e700537f1f484e672038743326c0ccea50a943e
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-31 22:15:07 +0530

    It returns the count of even numbers in the given range and TC : O(Q.N) and SC : O(N+Q)

---
+++ src/PrefixSum/EvenNumbersInARange.java
@@ -0,0 +1,56 @@
+package PrefixSum;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EvenNumbersInARange
+{
+    public int[] solve(int[] A, int[][] B) {
+        int Q = B.length;
+
+        int[] ans = new int[Q];
+
+
+        for (int i = 0; i < Q; i++) {
+            int count = 0;
+            // Get the start and end indices from B
+            int start = B[i][0];
+            int end = B[i][1];
+
+
+
+            for (int j = start; j <= end; j++) {
+                if (A[j] % 2 == 0) {
+                    count++;
+                }
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println("Enter the range M: ");
+        int Q = sc.nextInt();
+
+        int[] A = new int[N];
+        System.out.println("Enter the elements of A: ");
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        int[][] B = new int[Q][2];
+        System.out.println("Enter the elements of B: ");
+        for(int i = 0; i < Q; i++){
+            B[i][0]= sc.nextInt();
+            B[i][1] = sc.nextInt();
+        }
+
+        EvenNumbersInARange r = new EvenNumbersInARange();
+        System.out.println(Arrays.toString(r.solve(A, B)));
+
+        sc.close();
+    }
+}
