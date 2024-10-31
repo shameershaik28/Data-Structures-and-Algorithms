787fbc39523021b53937e754f351c62f3725c948
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-31 19:51:59 +0530

    It returns the prefix sum array without using an additional array and the TC : O(N) and SC: O(1)

---
+++ src/PrefixSum/PrefixSum.java
@@ -0,0 +1,32 @@
+package PrefixSum;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrefixSum {
+    public int[] solve(int[] A) {
+        int N = A.length;
+
+        for(int i =1; i<N ; i++)
+        {
+            A[i] = A[i-1]+A[i];
+        }
+        return A;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int N = sc.nextInt();
+
+        int[] A = new int[N];
+System.out.println("Enter the elements in the array: ");
+        for(int i = 0; i < N; i++)
+        {
+            A[i]= sc.nextInt();
+        }
+
+        PrefixSum ps = new PrefixSum();
+        System.out.println(Arrays.toString(ps.solve(A)));
+    }
+}
