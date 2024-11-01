4d7f960c85d339bce1133a8af69b4222cee91f12
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-01 18:29:48 +0530

    feat: Implement odd Numbers Count in Range
    - Added functionality to count odd numbers in a given range using prefix sums.
    - Optimized to achieve O(N + Q) time complexity, where N is the number of elements in the array and Q is the number of queries.
    - Utilizes O(N + Q) space complexity for storing prefix sums and results.
    
    Time Complexity (TC): O(N + Q)
    Space Complexity (SC): O(N + Q)

---
+++ src/PrefixSum/SumOfOddIndices.java
@@ -0,0 +1,64 @@
+package PrefixSum;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumOfOddIndices {
+    public int[] solve(int[] A, int[][] B) {
+        int N = A.length;
+        int Q = B.length;
+
+        int[] ans = new int[Q];
+        int[] pf= new int[N];
+
+        pf[0]=0;
+
+        for(int i=1; i<N; i++){
+            if(i%2!=0)
+            {
+                pf[i]= pf[i-1]+A[i];
+            }
+            else
+            {
+                pf[i]=pf[i-1];
+            }
+        }
+
+        for(int i =0; i<Q; i++)
+        {
+            int s= B[i][0];
+            int e = B[i][1];
+
+            if(s==0)
+            {
+                ans[i]= pf[e];
+            }
+            else {
+                ans[i]= pf[e]-pf[s-1];
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the  size of the array: ");
+        int N = sc.nextInt();
+        System.out.println("Enter the Number M: ");
+        int M = sc.nextInt();
+
+        int[] A = new int[N];
+        System.out.println("Enter the elements of the A array: ");
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        System.out.println("Enter the B elements : ");
+        int[][] B = new int[M][2];
+        for (int i = 0; i < M; i++) {
+            B[i][0] = sc.nextInt();
+            B[i][1] = sc.nextInt();
+        }
+        SumOfOddIndices s = new SumOfOddIndices();
+        System.out.println(Arrays.toString(s.solve(A, B)));
+
+    }
+}
