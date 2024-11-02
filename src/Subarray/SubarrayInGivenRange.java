4c583f3859bccbab78c6c580dab7f97038c31ec7
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-02 19:30:51 +0530

    Implemented Subarray extraction from given range
    - Added method to extract a subarray from array A between indices B and C.
    - Time Complexity: O(N) in the worst case (when extracting the entire array).
    - Space Complexity: O(N) in the worst case (when storing the entire array in the new subarray).

---
+++ src/Subarray/SubarrayInGivenRange.java
@@ -0,0 +1,34 @@
+package Subarray;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SubarrayInGivenRange {
+    public int[] solve(int[] A, int B, int C) {
+
+        int[] ans = new int[C-B+1];
+        for(int i =B; i<=C; i++)
+        {
+            ans[i-B]=A[i];
+        }
+        return ans;
+    }
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the array");
+    int N = sc.nextInt();
+
+    int[] A = new int[N];
+    System.out.println("Enter the elements of the array");
+    for (int i = 0; i < N; i++) {
+        A[i] = sc.nextInt();
+    }
+    System.out.println("Enter the start index B : ");
+    int B = sc.nextInt();
+    System.out.println("Enter the end index C : ");
+    int C = sc.nextInt();
+
+    SubarrayInGivenRange s = new SubarrayInGivenRange();
+    System.out.println(Arrays.toString(s.solve(A, B, C)));
+}
+}
