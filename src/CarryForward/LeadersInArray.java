ee95ad70dca7f24a6f7200392d275df3cdbdde77
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-04 19:09:49 +0530

    feat: Add LeadersInArray solution using carry forward technique
    Implemented LeadersInArray class to find all leader elements in an array.
    Leaders are elements greater than all elements to their right.
    Algorithm uses carry forward technique with a reverse traversal and maintains
    a running max to identify leaders efficiently.
    
    - Time Complexity: O(N) due to single pass over array of size N.
    - Space Complexity: O(N) to store the result in an ArrayList of leaders.
    
    Includes main method for user input and result display.

---
+++ src/CarryForward/LeadersInArray.java
@@ -0,0 +1,40 @@
+package CarryForward;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LeadersInArray {
+    public ArrayList<Integer> solve(ArrayList<Integer> A) {
+        int N = A.size();
+
+        ArrayList<Integer> ans  = new ArrayList<>();
+        int max = A.get(N-1);
+        ans.add(max);
+
+        for(int i =N-2; i>=0; i--) {
+            int temp = A.get(i);
+            if (temp>max)
+            {
+                ans.add(temp);
+                max = temp;
+            }
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int N = sc.nextInt();
+
+        ArrayList<Integer> A = new ArrayList<>();
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < N; i++) {
+            A.add(sc.nextInt());
+        }
+
+        LeadersInArray l = new LeadersInArray();
+        ArrayList<Integer> ans = l.solve(A);
+        System.out.println(ans);
+    }
+}
