1ae8db69f4699bf944af08f2d14c07917ed90e29
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-04 20:24:00 +0530

    Add solution to count switches for turning all bulbs on
    - Implemented a method to determine the number of switches needed to turn all bulbs on given their initial states.
    - Utilizes an ArrayList to store bulb states and iterates through the list to count necessary switches.
    
    Time Complexity (TC): O(N) where N is the number of bulbs in the input array.
    Space Complexity (SC): O(1) as only a constant amount of extra space is used.

---
+++ src/CarryForward/Bulbs.java
@@ -0,0 +1,37 @@
+package CarryForward;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Bulbs {
+    public int bulbs(ArrayList<Integer> A) {
+        int N = A.size();
+        int counter = 0;
+        int state =0;
+
+        for(int i = 0; i < N; i++) {
+
+            if(A.get(i) ==state)
+            {
+                counter++;
+                state = 1-state;
+            }
+        }
+        return counter;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int N = sc.nextInt();
+
+        ArrayList<Integer> A = new ArrayList<>();
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < N; i++) {
+            A.add(sc.nextInt());
+        }
+
+        Bulbs b = new Bulbs();
+        int ans = b.bulbs(A);
+        System.out.println(ans);
+    }
+}
