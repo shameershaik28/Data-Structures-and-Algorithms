b49e32e31055f0491a017d56732089d4b8bc094f
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-05 21:28:56 +0530

    feat: Add GoodPair solution to find pairs with a given sum in an array
    - Implemented solve method to find if there exists a pair (i, j) such that A[i] + A[j] = B.
    - Used a nested loop to check every unique pair in the array.
    - Returns 1 if a pair is found, otherwise returns 0.
    
    Time Complexity: O(N^2) - The nested loop structure results in checking all pairs, leading to quadratic time complexity.
    Space Complexity: O(1) - The solution uses constant space, with no additional data structures dependent on the input size.

---
+++ src/Arrays/IntroductionToArrays/GoodPair.java
@@ -0,0 +1,41 @@
+package Arrays.IntroductionToArrays;
+
+import java.util.Scanner;
+
+public class GoodPair {
+    public int solve(int[] A, int B) {
+        int N = A.length;
+
+        for(int i =0; i<N ; i++)
+        {
+            for(int j = i+1; j<N; j++)
+            {
+                if(i!=j && A[i]+A[j] == B)
+                {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+}
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Size of the array: ");
+        int N = input.nextInt();
+
+        int[] A = new int[N];
+System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < N; i++)
+        {
+            A[i] = input.nextInt();
+        }
+        System.out.println("Enter the element B: ");
+        int B = input.nextInt();
+
+        GoodPair gp = new GoodPair();
+        int ans = gp.solve(A, B);
+        System.out.println(ans);
+
+    }
+}
