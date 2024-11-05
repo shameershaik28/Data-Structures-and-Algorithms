c865b646207021b70b3537a7ffffe2c32e3fffa3
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-05 22:10:35 +0530

    Add solution for finding sum of max and min in an array
    - Implemented solve method to find the maximum and minimum elements in an array and return their sum.
    - Optimized by combining max and min checks into a single loop for efficiency.
    
    Time Complexity: O(N), where N is the length of the array, as we iterate over the array once.
    Space Complexity: O(1), since only a constant amount of additional space is used.

--- src/Arrays/IntroductionToArrays/MaxMinOfAnArray.java
+++ src/Arrays/IntroductionToArrays/MaxMinOfAnArray.java
@@ -4,10 +4,37 @@ import java.util.Scanner;
 
 public class MaxMinOfAnArray {
     public int solve(int[] A) {
+        int N = A.length;
+
+        if (N == 0) return 0;
+        int Max = Integer.MIN_VALUE;
+        int Min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            if (A[i] > Max) {
+                Max = A[i];
+            }
+            if (A[i] < Min) {
+                Min = A[i];
+            }
+        }
+
+        int sum = Max+Min;
+        return sum;
 
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int N = input.nextInt();
 
+        int[] A = new int[N];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < N; i++) {
+            A[i] = input.nextInt();
+        }
+
+        MaxMinOfAnArray m = new MaxMinOfAnArray();
+        int ans = m.solve(A);
+        System.out.println("The total sum is : "+ ans);
     }
 }
