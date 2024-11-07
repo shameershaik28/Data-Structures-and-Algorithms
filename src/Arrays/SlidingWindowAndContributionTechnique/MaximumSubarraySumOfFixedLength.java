24d38d8b05085cce7a7cbede03652924d1d8942f
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-07 18:50:01 +0530

    Implement sliding window approach to find maximum subarray sum of fixed length
    - Applied sliding window technique to find the maximum sum of any subarray of length B in the given array A.
    - Calculated the sum of the initial subarray of length B and used it as the starting maximum.
    - Used a sliding window to update the sum by removing the element at the start and adding the next element at each step.
    - Updated the maximum sum whenever a new window's sum exceeded the current maximum.
    - Added user input handling for array and subarray length (B) for easy testing.
    
    Time Complexity (TC): O(N), where N is the length of the input array. Calculating the sum of the first window takes O(B) time, and updating the sum for each subsequent window is done in O(1). Thus, the overall complexity is O(N).
    
    Space Complexity (SC): O(1)

---
+++ src/Arrays/SlidingWindowAndContributionTechnique/MaximumSubarraySumOfFixedLength.java
@@ -0,0 +1,42 @@
+package Arrays.SlidingWindowAndContributionTechnique;
+
+import java.util.Scanner;
+
+//Contribution Technique Approach
+public class MaximumSubarraySumOfFixedLength {
+    public long solve(int[] A, int B) {
+        int N = A.length;
+        long max = Integer.MIN_VALUE;
+        long sum = 0;
+        for(int i = 0; i < B; i++){
+            sum += A[i];
+        }
+        max = Math.max(max, sum);
+        int s = 0, e = B;  // start of window is 0, end is B
+        while (e < N) {
+            sum = sum - A[s]+ A[e];
+            max = Math.max(max, sum);
+            s++;
+            e++;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the  array");
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        System.out.println("Enter the B:");
+        int B = sc.nextInt();
+
+        MaximumSubarraySumOfFixedLength s = new MaximumSubarraySumOfFixedLength();
+        long ans = s.solve(A, B);
+        System.out.println(ans);
+
+    }
+
+}
