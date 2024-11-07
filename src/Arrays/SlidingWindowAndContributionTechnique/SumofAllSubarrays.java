1fc3834707332a84384135bcac0e0556937dd798
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-07 16:33:25 +0530

    Implement prefix sum approach to calculate the sum of all subarrays
    - The solution first computes a prefix sum array, where each element at index i contains the sum of the array elements from the start up to index i.
    - For each subarray, the sum is computed using the prefix sum array for efficiency:
      - If the subarray starts at index 0, the sum is directly taken from the prefix sum at index e.
      - Otherwise, the sum is calculated as the difference between two prefix sums.
    - The time complexity of this approach is O(N^2), due to the two nested loops for calculating the sum of each subarray.
    - The space complexity is O(N), because the prefix sum array of size N is stored in addition to the input array.

--- src/Arrays/SlidingWindowAndContributionTechnique/SumofAllSubarrays.java
+++ src/Arrays/SlidingWindowAndContributionTechnique/SumofAllSubarrays.java
@@ -2,20 +2,30 @@ package Arrays.SlidingWindowAndContributionTechnique;
 
 import java.util.Scanner;
 
-//Bruce-Force Approach
+//Prefix sum Approach
 public class SumofAllSubarrays {
-    public long subarraySum(int[] A) {
-        int N = A.length;
-        long ans = 0;
-        for(int s=0; s<N; s++){
-            int sum =0;
-            for(int e=s; e<N; e++){
-                   sum+=A[e];
-                   ans+=sum;
-               }
-            }
-        return ans;
-    }
+   public long subarraySum(int[] A) {
+       int N = A.length;
+       long total = 0;
+       int[] pf= new int[N];
+       pf[0]=A[0];
+
+       for(int i=1;i<N;i++)
+       {
+           pf[i]=pf[i-1]+A[i];
+       }
+
+       for(int s=0;s<N;s++)
+       {
+           for(int e=s;e<N;e++)
+           {
+              long sum = (s==0) ? pf[e] : pf[e] - pf[s-1];
+              total += sum;
+           }
+       }
+       return total;
+   }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
