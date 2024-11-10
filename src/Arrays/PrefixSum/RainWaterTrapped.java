43b9377f2e7f9a2ecb801241506c049732a5f5fd
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-10 13:19:04 +0530

    feat: Implement Rain Water Trapping algorithm using Prefix Sum approach
    - Implemented RainWaterTrapped class with 	rap method to calculate the amount of rainwater that can be trapped between heights using an array of integers.
    - Used the Prefix Sum approach to store maximum heights to the left and right of each element.
    - Included a main method to take input from the user and display the result.
    
    Approach: Prefix Sum Array
    Time Complexity (TC): O(N), where N is the length of the array, as we traverse the array three times.
    Space Complexity (SC): O(N), for storing the left and right max arrays.

---
+++ src/Arrays/PrefixSum/RainWaterTrapped.java
@@ -0,0 +1,57 @@
+package Arrays.PrefixSum;
+
+import java.util.Scanner;
+
+public class RainWaterTrapped {
+    public int trap(final int[] A) {
+        int N = A.length;
+        int ans =0; // final result
+        int[] pSum = new int[N];
+        pSum[0] = A[0];
+        int pmax = A[0];
+        for(int i =1; i<N; i++){
+            if(A[i]>pmax)
+            {
+                pmax=A[i];
+            }
+            pSum[i]= pmax;
+        }
+
+        int[] sSum= new int[N];
+         sSum[N-1] = A[N-1];
+         int sMax=A[N-1];
+        for(int i =N-2; i>=0; i--){
+            if(A[i]>sMax)
+            {
+                sMax=A[i];
+            }
+            sSum[i]= sMax;
+        }
+
+        for(int i=0; i<N; i++){
+            int left = pSum[i];
+            int right = sSum[i];
+            int min = Math.min(left,right);
+            int amount = min -A[i];
+            ans += amount;
+        }
+        return ans;
+}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int N = sc.nextInt();
+
+        int[] A = new int[N];
+        System.out.println("Enter the elements of array: ");
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        RainWaterTrapped solution = new RainWaterTrapped();
+        int ans = solution.trap(A);
+        System.out.println(ans);
+
+    }
+}
