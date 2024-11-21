7e505c3c52a470aab50d98dcc57289208144825f
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-21 16:14:15 +0530

    feat: Add solution to reverse a string using two-pointer approach
    Added a function to reverse a given string using the two-pointer technique. The solution efficiently swaps characters from both ends of the string until the middle is reached, leveraging a char array for immutability handling.
    
    Approach:
    - Convert the input string to a char array.
    - Use two pointers (s and e) initialized at the start and end of the array.
    - Swap characters at s and e while incrementing s and decrementing e until they meet.
    - Construct and return a new string from the modified char array.
    
    Time Complexity: O(N), where N is the length of the input string.
    Space Complexity: O(N), due to the creation of a char array.

---
+++ src/Strings/SimpleReverse.java
@@ -0,0 +1,30 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class  SimpleReverse {
+    public String solve(String A) {
+
+        char[] ans = A.toCharArray();
+        int N = ans.length;
+        int s =0;
+        int e = N-1;
+
+        while(s<e)
+        {
+            int temp = ans[s];
+            ans[s] = ans[e];
+            ans[e] = (char) temp;
+            s++;
+            e--;
+        }
+        return new String(ans);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        SimpleReverse l = new SimpleReverse();
+        System.out.println(l.solve(s));
+    }
+}
