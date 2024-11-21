e42a690850bfdeac841fecf130300050918fe203
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-21 16:20:58 +0530

    feat: Optimize string reversal using StringBuilder for in-place operations
    Replaced the character array approach with an optimized solution using StringBuilder to reverse a string in-place. This approach leverages the mutability of StringBuilder for efficient character swapping.
    
    Approach:
    - Initialize a StringBuilder with the input string
    - Use two pointers (s and e) for swapping characters at the start and end of the string
    - Increment the start pointer and decrement the end pointer until they meet
    - Return the reversed string using the StringBuilder's toString method
    
    Time Complexity: O(N), where N is the length of the input string
    Space Complexity: O(N), due to the StringBuilder storage for the input string

--- src/Strings/SimpleReverse.java
+++ src/Strings/SimpleReverse.java
@@ -3,23 +3,23 @@ package Strings;
 import java.util.Scanner;
 
 public class  SimpleReverse {
-    public String solve(String A) {
+        public String solve(String A) {
+            StringBuilder sb = new StringBuilder(A);
+            int s = 0;
+            int e = sb.length() - 1;
 
-        char[] ans = A.toCharArray(); // convert it to character array
-        int N = ans.length;
-        int s =0;
-        int e = N-1;
+            while (s < e) {
+                // Swap characters in-place
+                char temp = sb.charAt(s);
+                sb.setCharAt(s, sb.charAt(e));
+                sb.setCharAt(e, temp);
 
-        while(s<e)
-        {
-            int temp = ans[s];
-            ans[s] = ans[e];
-            ans[e] = (char) temp;
-            s++;
-            e--;
+                s++;
+                e--;
+            }
+
+            return sb.toString(); // Return the reversed string
         }
-        return new String(ans);
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string: ");
