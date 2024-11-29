d9a5e9ca58dfe3a2fda874f2aab6e92df43963e6
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-11-29 17:31:59 +0530

    feat: Add recursive solution to print numbers from 1 to A
    - Added ecursive(int A) to print numbers from 1 to A using recursion.
    - solve(int A) calls ecursive(A) and adds a newline for better formatting.
    
    ### TC & SC:
    - Time Complexity: O(n)
    - Space Complexity: O(n)

--- src/Recursion/Print1ToAFunction.java
+++ src/Recursion/Print1ToAFunction.java
@@ -4,15 +4,19 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Print1ToAFunction {
-    public void solve(int A) {
-        //Base case
-        if(A==1)
-        {
-            System.out.print(A+" ");
-            return; //It goes back to the function when it has been called.
+    public void recursive(int A) {
+        // Base case
+        if (A == 1) {
+            System.out.print(A + " ");
+            return; // Goes back to the function when it has been called.
         }
-        solve(A-1); //It calls the recursive function
-        System.out.print(A+" ");
+        recursive(A - 1); // Calls the recursive function
+        System.out.print(A + " ");
+    }
+
+    public void solve(int A) {
+        recursive(A); // Calls the recursive function
+        System.out.println(); // After printing Everything It prints a new Line at the end.
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
