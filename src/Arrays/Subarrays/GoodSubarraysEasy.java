4b1a759d2d6542f5324df25165c379f48183b97a
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-12-31 16:43:34 +0530

    Implemented solution for counting good subarrays using prefix sum approach
    - Added a method solve to count subarrays that satisfy two conditions:
      1. Even-length subarrays with a sum less than a given value B.
      2. Odd-length subarrays with a sum greater than B.
    - Utilized a prefix sum array to efficiently compute the sum of subarrays in O(1) time.
    - The solution uses nested loops to check all possible subarrays, resulting in a time complexity of O(n²).
    - The space complexity is O(n) due to the prefix sum array.

--- src/Arrays/Subarrays/GoodSubarraysEasy.java
+++ src/Arrays/Subarrays/GoodSubarraysEasy.java
@@ -5,41 +5,47 @@ import java.util.Scanner;
 
 //Brute-Force Approach
 public class GoodSubarraysEasy {
-        public int solve(ArrayList<Integer> A, int B) {
-            int count = 0, left = 0, sum = 0;
-
-            for (int right = 0; right < A.size(); right++) {
-                sum += A.get(right);
-
-                while (sum >= B && left <= right) {
-                    sum -= A.get(left);
-                    left++;
+        public int solve(int[] A, int B) {
+            int n = A.length;
+            int[] pref = new int[n];
+            pref[0] = A[0];
+            int ans = 0;
+            for(int i = 1 ; i < n ; i++){
+                pref[i] = pref[i - 1] + A[i];
+            }
+            for(int i = 0 ; i < n ; i++){
+                for (int j = i ; j < n ; j++){
+                    int sz = j - i + 1;
+                    int sum;
+                    if(i == 0){
+                        sum = pref[j];
+                    }
+                    else{
+                        sum = pref[j] - pref[i - 1];
+                    }
+                    if(sz % 2 == 0 && sum < B)ans++;
+                    if(sz % 2 == 1 && sum > B)ans++;
                 }
-
-                // All subarrays ending at 'right' and starting from any index 'left' to 'right'
-                count += (right - left + 1);
             }
-
-            return count;
+            return ans;
         }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int N = sc.nextInt();
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the size of the array");
-            int N = sc.nextInt();
+        int[] A = new int[N];
 
-            ArrayList<Integer> A = new ArrayList<>();
-
-            System.out.println("Enter the elements of the array");
-            for(int i=0; i<N; i++)
-            {
-                A.add(sc.nextInt());
-            }
-            System.out.println("Enter the sum B : ");
-            int B = sc.nextInt();
-            GoodSubarraysEasy solution = new GoodSubarraysEasy();
-            // Calling solve function and printing the result
-            int result = solution.solve(A, B);
-            System.out.println("Number of subarrays with sum less than " + B + " is: " + result);
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
         }
+        System.out.println("Enter the sum B: ");
+        int B = sc.nextInt();
+
+        GoodSubarraysEasy solution = new GoodSubarraysEasy();
+        // Calling solve function and printing the result
+        int result = solution.solve(A, B);
+        System.out.println("Number of good subarrays is: " + result);
+    }
     }
