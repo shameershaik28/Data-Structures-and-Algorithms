efdbd4f4c9e895f3fa9a11e99b977ba4818111e6
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2025-01-01 12:25:44 +0530

    Implemented solution to count vowels in specified ranges of a string
    - Added method to solve the problem using a 2D array of ranges
    - Iterated over each range to count vowels ('a', 'e', 'i', 'o', 'u') in the substring
    - Optimized input handling and output display
    - Included necessary imports and made the solution interactive with user input
    
    Time Complexity (TC): O(M * N), where M is the number of queries and N is the length of the string.
    Space Complexity (SC): O(N + M), where N is the length of the string and M is the number of queries.

---
+++ src/Arrays/PrefixSum/VowelsInARange.java
@@ -0,0 +1,51 @@
+package Arrays.PrefixSum;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class VowelsInARange {
+    public int[] solve(String A, int[][] B) {
+        int N = A.length();
+        int M = B.length;
+
+        if(M==0|| N==0)
+            return new int[0];
+
+
+        int[] ans= new int[M];
+        for(int i=0; i<M; i++)
+        {
+            int count=0;
+            int start= B[i][0];
+            int end = B[i][1];
+
+            for(int j=start; j<=end; j++)
+            {
+                if(A.charAt(j)=='a'||A.charAt(j)=='e'||A.charAt(j)=='i'||A.charAt(j)=='o'||A.charAt(j)=='u')
+                {
+                    count++;
+                }
+            }
+            ans[i]= count;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String A = in.nextLine();
+        System.out.println("Enter the rows : ");
+        int M = in.nextInt();
+
+        int[][] B = new int[M][2];
+        System.out.println("Enter the elements in 2d array: ");
+        for(int i = 0 ; i < M ; i++){
+            B[i][0] = in.nextInt();
+            B[i][1] = in.nextInt();
+        }
+        VowelsInARange v = new VowelsInARange();
+        int[] ans = v.solve(A, B);
+        System.out.println(Arrays.toString(ans));
+    }
+}
