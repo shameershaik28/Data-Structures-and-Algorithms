36b6d214d9e5cf41098a961c4962f672bb4e765e
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2025-01-01 16:30:36 +0530

    feat: Implement prefix maximum array calculation
    - Added a solution to compute the prefix maximum array for a given input array.
    - Utilized a single pass with O(N) time complexity and O(N) space complexity.
    - Ensured correctness by maintaining a running maximum during iteration.

--- src/Arrays/PrefixSum/PrefixMaximum.java
+++ src/Arrays/PrefixSum/PrefixMaximum.java
@@ -6,26 +6,32 @@ import java.util.Scanner;
 public class PrefixMaximum {
     public int[] solve(int[] A) {
         int N = A.length;
-        int max = Integer.MIN_VALUE;
+        int max = A[0];
 
-        for(int i=0; i<N;i++)
+        int[] prefixSum = new int[N];
+        prefixSum[0] = A[0];
+        for(int i=1; i<N;i++)
         {
             if(A[i]>max)
             {
+                prefixSum[i] = A[i];
                 max = A[i];
             }
-            if(A[i]<max)
+            else
             {
-                A[i]=max;
+                prefixSum[i] = prefixSum[i-1];
             }
+
         }
-        return A;
+        return prefixSum;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
         int N = sc.nextInt();
 
         int[] arr = new int[N];
+        System.out.println("Enter the elements of the array: ");
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
