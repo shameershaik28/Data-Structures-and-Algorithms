cc9e1174e4c63cf41231fce341a001d62365122e
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2025-01-13 14:14:34 +0530

    Here’s an example of a well-structured Git commit message:
    vbnet
    Copy code
    feat: Implement Sieve of Eratosthenes to count primes up to A
    
    - Added the Sieve of Eratosthenes algorithm to count the number of primes up to the given number A.
    - Initialized a boolean array to track prime numbers and implemented loops to mark multiples as non-prime.
    - Included logic to count all primes after sieve processing.
    
    Time Complexity: O(A * log(log(A)))
    - The outer loop iterates up to sqrt(A), and marking multiples of primes takes O(A * log(log(A))) overall.
    
    Space Complexity: O(A)
    - Requires a boolean array of size A+1 to track primality.

--- src/ProblemSolving/CountOfPrimes.java
+++ src/ProblemSolving/CountOfPrimes.java
@@ -3,26 +3,38 @@ package ProblemSolving;
 import java.util.Scanner;
 
 public class CountOfPrimes {
-    public int factors(int A) {
-        int primeCount = 0;
-        for(int i =2; i<=A; i++) {
-            int count=0;
-            for (int j = 1; j * j <= i; j++) {
-                if (i % j == 0) {
-                    if (j == i / j) {
-                        count = count + 1;
-                    } else {
-                        count += 2;
-                    }
+    public int solve(int A) {
+
+        if (A <= 1) {
+            return 0;
+        }
+
+        // Initialize the boolean array
+        boolean[] isPrime = new boolean[A + 1];
+
+        // Mark all numbers as prime initially
+        for (int i = 2; i <= A; i++) {
+            isPrime[i] = true;
+        }
+
+        // Implement the Sieve of Eratosthenes
+        for (int i = 2; i * i <= A; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= A; j += i) {
+                    isPrime[j] = false;
                 }
             }
-            if(count == 2)
-            {
-                primeCount++;
+        }
+
+        // Count the total number of primes
+        int totalCount = 0;
+        for (int i = 2; i <= A; i++) {
+            if (isPrime[i]) {
+                totalCount++;
             }
         }
-        return primeCount;
 
+        return totalCount;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -31,7 +43,7 @@ public class CountOfPrimes {
 
 
         CountOfPrimes ob = new CountOfPrimes();
-        int P = ob.factors(n);
+        int P = ob.solve(n);
         System.out.println("Number of primes up to " + n + ": " + P);
     }
 }
